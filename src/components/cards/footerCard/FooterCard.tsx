ee325844f9d1f5a9d82eda43535fcf084a7022a2
Facundodandrea <facundodandrea01@gmail.com>
2024-10-26 14:31:53 -0300

    fix: :bug: change a footer

--- src/components/cards/footerCard/FooterCard.tsx
+++ src/components/cards/footerCard/FooterCard.tsx
@@ -3,11 +3,29 @@ import './footerCard.css'
 const FooterCard = () => {
   return (
     <div className='footer-card'>
-      <p>© 2024 Diseñado por UX Ana Rangel</p>
+      <p>
+        © 2024 Diseñado por{' '}
+        <a
+          href='https://www.linkedin.com/in/ux-ana-rangel/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          UX Ana Rangel
+        </a>
+      </p>
       <div className='made-with'>
         <p>Hecho con</p>
         <video src='/assets/video/heart.webm' autoPlay loop muted />
-        <p>por la comunidad</p>
+        <p>
+          por{' '}
+          <a
+            href='https://facundodandrea.vercel.app'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Facundo D'Andrea
+          </a>
+        </p>
       </div>
     </div>
   )
